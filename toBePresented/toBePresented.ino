2f8785902ebcc92e99e8d03b1b80a04cf05db6a4
Aditya Kumar Singh <adityakumar2809@gmail.com>
2018-11-26 00:03:28 +0530

    display login menu

--- toBePresented/toBePresented.ino
+++ toBePresented/toBePresented.ino
@@ -1,4 +1,4 @@
-int printState=0, inputState=0;
+int printState=0, inputState=0, printRepeat=0, inputRepeat=0;
 char input=NULL;
 void setup() 
 {
@@ -7,54 +7,58 @@ void setup()
 
 void loop() 
 {
-  if(printState==0)
-  {
-    loginMenu();
-  }
-  takeInput();
-  if(printState==1 && inputState==1)
-  {
-    printStuff();
-  }
+  printStuff();
+  inputStuff();
 }
 
-
-void loginMenu()
+void inputStuff()
 {
-  Serial.println("How do you want to login?\n\n");
-  Serial.println("1.User\n");
-  Serial.println("2.Service Center In-Charge\n");
-  Serial.println("3.Pollution Control Board Representative\n\n");
-  Serial.println("Please enter your choice");  
-  printState=1;
-}
-
-void takeInput()
-{
-  if(Serial.available()>0)
+  if(inputState==0)
   {
-    input=Serial.read();
-    inputState=1;
+    if(Serial.available()>0)
+    {
+      input=Serial.read();
+      inputState=1;
+      printRepeat=0;
+    }
   }
 }
+
+
 
 void printStuff()
 {
-  if(input=='1')
+  if(printRepeat==0)
   {
-    Serial.println("You want to login as a user");
+      if(printState==0)
+      { 
+        Serial.println("How do you want to login?\n\n");
+        Serial.println("1.User\n");
+        Serial.println("2.Service Center In-Charge\n");
+        Serial.println("3.Pollution Control Board Representative\n\n");
+        Serial.println("Please enter your choice");  
+        printState=1;
+      }
+      else if(printState==1)
+      {
+          if(input=='1')
+          {
+            Serial.println("You want to login as a user");
+          }
+          else if(input=='2')
+          {
+            Serial.println("You want to login as a Service Center In-Charge");
+          }
+          else if(input=='3')
+          {
+            Serial.println("You want to login as a Pollution Control Board Representative");
+          }
+          else if(input!=NULL)
+          {
+            Serial.println("Wrong Input\nRedirecting to Home Page");
+          }
+          printState=2;
+      }
+      printRepeat=1;
   }
-  else if(input=='2')
-  {
-    Serial.println("You want to login as a Service Center In-Charge");
-  }
-  else if(input=='3')
-  {
-    Serial.println("You want to login as a Pollution Control Board Representative");
-  }
-  else if(input!=NULL)
-  {
-    Serial.println("Wrong Input\nRedirecting to Home Page");
-  }
-  printState=2;
 }
