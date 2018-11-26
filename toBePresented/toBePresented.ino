cd45ae435f1e6c9a17c427ba27d75d919ef6fad7
Aditya Kumar Singh <adityakumar2809@gmail.com>
2018-11-27 01:48:05 +0530

    display user section

--- toBePresented/toBePresented.ino
+++ toBePresented/toBePresented.ino
@@ -1,7 +1,17 @@
+//PIN CONNECTIONS
+int sensor = A4;
+
+int lockState=0, limSensorReading=-1024, timeLeft=300, timeExtended=120;
+
+//MENU 0 VARIABLES
 int printState=0, inputState=0, printRepeat=0, inputRepeat=0;
-//char input=NULL;
 char input[20], temp, id[20], password[20], user_id[20]="user@advec", user_password[20]="user@123", ss_id[20]="ss@advec", ss_password[20]="ss@123", pcb_id[20]="pcb@advec", pcb_password[20]="pcb@123";
-int loginType=0, i=0, temp2=0, check=0, tookPass=0;
+int loginType=0, i=0, temp2=0, check=0, tookPass=0, menuStatus=1;
+
+//MENU 1 VARIABLES
+int userPrintRepeat=0, userPrintState=0, userInputState=0, userChoice=0, userSensorReading, userTimeExtendPerm=0;
+char veh_id[15]="PB11 A 1111";
+
 void setup() 
 {
  Serial.begin(9600);
@@ -9,14 +19,24 @@ void setup()
 
 void loop() 
 {
-  printStuff();
-  inputStuff();
-  if(check==1 && tookPass==1)
+  if(menuStatus==0)
   {
-    checkCredentials();
+    printStuff();
+    inputStuff();
+    if(check==1 && tookPass==1)
+    {
+      checkCredentials();
+    }
+  }
+  else if(menuStatus==1)
+  {
+    userPrintStuff();
+    userInputStuff();
   }
 }
 
+
+//MENU 0 STARTS
 void inputStuff()
 {
   if(inputState==0)
@@ -155,6 +175,7 @@ void checkCredentials()
       Serial.println("\nLogin successfull\n");
       check=0;
       tookPass=0;
+      menuStatus=1;
     }
     else
     {
@@ -195,5 +216,101 @@ void checkCredentials()
       
   }
 }
+     
+
+//MENU 1 STARTS
+
+void userPrintStuff()
+{
+  if(userPrintRepeat==0)
+  {
+    if(userPrintState==0)
+    {
+      Serial.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nWELCOME TO USER PORTAL");
+      Serial.print("\nThe registration number of your vehicle is ");
+      Serial.println(veh_id);
+      Serial.println("\nChoose an action to perform\n");
+      Serial.println("1.Display current emission levels\n");
+      Serial.println("2.Logout\n");
+      if(lockState==1 && userTimeExtendPerm==0)
+      {
+        Serial.println("3.Request time\n");
+      }
       
-  
+      userPrintState++;
+    }
+    else if(userPrintState==1)
+    {
+      if(userChoice==1)
+      {
+        Serial.print("The current emission level of your vehicle is ");
+        userSensorReading=analogRead(sensor);
+        Serial.println(userSensorReading);
+        if(userSensorReading>limSensorReading)
+        {
+          Serial.print("The current emission level of your vehicle is higher than the limiting emission level which stands at ");
+          Serial.println(limSensorReading);
+          if(lockState==1)
+          {
+            Serial.println("\nYour vehicle is currently forced to come to halt");
+          }
+          else
+          {
+            Serial.print("\nYour vehicle will be forced to come to halt in ");
+            Serial.print(timeLeft);
+            Serial.println(" seconds\n");
+          }
+          Serial.println("Kindly move your vehicle to the nearest Service Center as soon as possible");
+        }
+        Serial.println("\nRedirecting to Home Page in 5 seconds");
+        delay(5000);
+        userPrintState=userInputState=userChoice=0;
+        userPrintRepeat=-1;
+      }
+      else if(userChoice==3 && userTimeExtendPerm==0)
+      {
+        Serial.print("\nYou have been granted an extended period of ");
+        Serial.println(timeExtended);
+        Serial.println("\nRedirecting to Home Page in 5 seconds");
+        delay(5000);
+        userPrintState=userInputState=userChoice=0;
+        userPrintRepeat=-1;
+        userTimeExtendPerm=1;
+      }
+      else
+      {
+        Serial.println("\nRedirecting to Home Page");
+        printState=inputState=printRepeat=inputRepeat=loginType=0;
+        userPrintState=userInputState=userPrintRepeat=userChoice=menuStatus=0;
+      }
+      
+    }
+    userPrintRepeat++;
+  }
+}
+
+
+void userInputStuff()
+{
+  if(userInputState==0)
+  {
+    while(Serial.available()>0)
+    {
+      temp=Serial.read();
+      if(temp!='\r' && i<19)
+      {
+        input[i++]=temp;
+      }
+      temp2=1;
+    }
+    if(temp2==1)
+    {
+      input[i]='\0';
+      i=0;
+      userInputState++;
+      userPrintRepeat--;
+      userChoice = atoi(input);
+      temp2=0;
+    }
+  }
+}
